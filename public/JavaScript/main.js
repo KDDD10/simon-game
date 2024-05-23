8d83800b0f82cedbac0b05c9743f507ba8c028ca
KDDD10 <kamarondaley@yahoo.co.uk>
2024-05-23 22:08:42 +0100

    testing git push

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -13,6 +13,7 @@ const arrayInGame = [];
 let highestScore = 0;
 let currentScore = 0;
 
+
 // fuction to reset game 
 function resetGame() {
     arrayMain.length = arrayInGame.length = currentScore = 0;
@@ -21,3 +22,160 @@ function resetGame() {
     startButton.innerText = "Start";
     disableBoxButton(true, true);
     disbleStartButton(false);
+}
+
+//disble a button box logic or UI style
+function disableBoxButton(isDisable, isDisabledUI) {
+    for (const element of document.getElementsByClassName("box")) {
+        element.disabled = isDisable;
+
+        if (isDisabledUI) element.classList.add("disable");
+        else element.classList.remove("disable");
+    }
+}
+// function to disable start button
+function disbleStartButton(enabler) {
+    startButton.disabled = enabler;
+
+    if (enabler) startButton.classList.add("disable");
+    else startButton.classList.remove("disable");
+}
+
+// add glow effect
+function activeBox(boxID) {
+    const box = document.getElementById(boxID);
+    box.classList.add("box_press");
+    playSound("coin");
+
+    setTimeout(() => {
+        box.classList.remove("box_press");
+    }, 400);
+}
+
+function addColorRandom() {
+    arrayMain.push(arrayBox[Math.round(Math.random() * 3)]);
+    
+}
+
+function playColorSet() {
+    startButton.innerText = "Wait";
+    addColorRandom();
+
+    //use for playing the next color set
+    let index = 0;
+    const glowEach = setInterval(() => {
+        activeBox(arrayMain[index++]);
+
+        if (index === arrayMain.length) {
+            startButton.innerText = "Guess";
+            disableBoxButton(false, false);
+            clearInterval(glowEach);
+        }
+    }, 600);
+}
+
+function checkColorSet() {
+    //lose
+    if (!isArrayStartsWith(arrayMain, arrayInGame)) loseLevel();
+    //win
+    else if (arrayInGame.length === arrayMain.length && arrayInGame.length !== 0) winLevel();
+}
+
+function loseLevel() {
+    playSound("lose");
+    showLostModal(true);
+    resetGame();
+}
+
+//reset ingame array and add one color randomly
+function winLevel() {
+    arrayInGame.length = 0;
+    levelText.innerText = `Current: ${(arrayMain.length + 1).toString()}`;
+    startButton.innerText = "Start";
+
+    disableBoxButton(true);
+    playSound("win");
+
+    setTimeout(() => {
+        disableBoxButton(true, true);
+        disbleStartButton(false);
+    }, 1000);
+
+    currentScore++;
+
+    if (currentScore > highestScore || highestScore === 0) {
+        scoreText.innerText = `highest : ${++highestScore}`;
+    }
+}
+
+//Function to play sounds 
+
+function playSound(soundName) {
+    let audio;
+    switch (soundName) {
+        case "coin":
+            audio = new Audio("resources/audios/coin_sound.wav");
+            break;
+        case "win":
+            audio = new Audio("resources/audios/win.wav");
+            break;
+        case "lose":
+            audio = new Audio("resources/audios/lose.wav");
+            break;
+        case "try":
+            audio = new Audio("resources/audios/try.wav");
+            break;
+        case "start":
+            audio = new Audio("resources/audios/start.wav");
+            break;
+        default:
+            return; 
+    }
+    audio.play()
+}
+
+function showLostModal(enabler) {
+    if (enabler) {
+        playSound("lose");
+        modal.classList.remove("hide");
+    } else {
+        modal.classList.add("hide");
+    }
+}
+
+function isArrayStartsWith(first, second) {
+    const firstArray = [...first];
+    const secondArray = [...second];
+
+    for (let index = 0; index < secondArray.length; index++)
+        if (firstArray[index] !== secondArray[index]) return false;
+
+    return true;
+}
+
+//function clicking all box
+for (const item of document.getElementsByClassName("box"))
+    item.addEventListener("click", () => {
+        const id = item.getAttribute("id");
+        arrayInGame.push(id);
+        checkColorSet();
+        playSound("coin");
+    });
+
+modalButton.addEventListener("click", () => {
+    playSound("try");
+    showLostModal(false);
+});
+
+startButton.addEventListener("click", () => {
+    disbleStartButton(true);
+    disableBoxButton(true, false);
+    playColorSet();
+    playSound("start");
+});
+
+// starting point
+resetGame();
+
+
+
